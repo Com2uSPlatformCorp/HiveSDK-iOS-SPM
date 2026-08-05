42d7b724508dca0d6bd7f8a7d4115acfe691e8eb
Com2us Platform <PC@com2us.com>
2026-08-05 17:19:18 +0900

    Upload New Version: 26.6.0-beta9

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 private let isDevMode = false
-private let packageVersion = "26.6.0-beta8"
+private let packageVersion = "26.6.0-beta9"
 
 let package = Package(
     name: "Hive",
